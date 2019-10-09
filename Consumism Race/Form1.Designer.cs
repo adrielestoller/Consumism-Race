42ab87aff1cd24183417a76ea5ef96dcb1515e65
Adriel <adrielestoller@nave.org.br>
2019-10-09 10:44:07 -0300

    Arquivos iniciais do projeto adicionados

---
+++ Consumism Race/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Consumism_Race
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.configButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(293, 713);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // configButton
+            // 
+            this.configButton.Location = new System.Drawing.Point(36, 341);
+            this.configButton.Name = "configButton";
+            this.configButton.Size = new System.Drawing.Size(215, 42);
+            this.configButton.TabIndex = 1;
+            this.configButton.Text = "CONFIGURAÇÕES";
+            this.configButton.UseVisualStyleBackColor = true;
+            this.configButton.Click += new System.EventHandler(this.ConfigButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.configButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Consumism Race";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button configButton;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,25 @@
+# Consumism-Race
+
+ Jogo Arcade onde durante a primeira guerra onde o o consumo toma controle das pessoas incentivado pela Pop Art, dois empresários rivais competem pela supremacia das comidas rápidas em partidas do jogo japonês Hasami Shogi.
+ 
+# Narrativa
+
+ É 1950, a bomba da “Guerra Fria” explodiu nos Estados Unidos e na Rússia impactando o mundo todo. Mas, não é isso que nossa história veio contar. 
+ Numa cidade chamada Preckshak, os moradores são muito calmos… ou quase. MC Duck e Burger Queen eram dois pequenos empresários que haviam descoberto o poder da comida rápida, eles até tiveram algo no passado mas não vem ao caso agora, só o que precisam saber é que eles não se davam muito bem. Os dois eram espetaculares em suas barracas criando receitas e molhos que deixavam as pessoas alucinadas com as explosões de sabores. Quando finalmente algum deles conseguia alguma vantagem em relação ao outro, o mesmo vinha e fazia algo tão bom quanto.
+ E então, em um dia qualquer, um empresário vindo do exterior procurando por novas mentes, se depara com esses dois gigantes de lanches e fica indeciso em qual escolher para abrir seu novo empreendimento. Com isso, propôs uma disputa (Hasami Shogi) e quem ganhasse teria seu apoio e patrocínio.
+
+# Mecânicas
+
+- Cada jogador posicionará suas 9 peças na primeira fileira do tabuleiro;
+- É possível mover as peças na direção horizontal e vertical (quantas casas quiser);
+- A cada rodada os jogadores têm direito a movimentar uma peça;
+- É possível capturar uma peça inimiga após o jogador conseguir bloquear um dos sentido de movimentação de uma ou mais peças;
+- Para capturar uma peça localizada no canto do tabuleiro, é preciso posicionar uma peça na vertical e outra na horizontal, de modo a bloquear sua movimentação;
+- O posicionamento de uma peça entre duas inimigas, já posicionadas, não constitui captura;
+- Se um jogador fizer o mesmo movimento três vezes seguidas, o jogador adversário será considerado vencedor.
+
+# Dinâmicas 
+
+- Perseguição de peças;
+- Aquisição territorial;
+
